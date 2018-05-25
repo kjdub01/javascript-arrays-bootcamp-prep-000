c4a719e6a042a659cad0e4bd3027854c757d342a
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-05-25 17:22:22 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,27 @@
+var chocolateBars = new Array ("snickers", "hundred grand", "kitkat", "skittles");
+
+function addElementToBeginningOfArray(array, element) {
+  return ["foo", ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+   array.unshift(element)
+   return array
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndgOfArrayToEndOfArray(array, element) {
+  array.push("foo")
+  return array
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  
+}
